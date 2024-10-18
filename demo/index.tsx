82aff59560f4d395f65c2a4b48de8b4659ac62da
Matthew Goodwin <49927862+m4ttheweric@users.noreply.github.com>
2024-10-18 11:42:14 -0500

    add prettier

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,31 +1,38 @@
-import * as React from 'react'
-import * as ReactDOM from 'react-dom'
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
 
 //import { Walktour, WalktourLogic } from 'walktour'; // test with npm pack
-import { Walktour, WalktourLogic } from '../src/components/Walktour'
-import { playgroundSetup, secondarySteps, primaryIntoSecondary } from './setup'
+import { Walktour, WalktourLogic } from '../src/components/Walktour';
+import { playgroundSetup, secondarySteps, primaryIntoSecondary } from './setup';
 
 const App = () => {
-
   const [tourOpen, setTourOpen] = React.useState<boolean>(true);
 
   return (
     <>
-      {playgroundSetup({ buttonText: "Toggle Tour", onButtonClick: () => setTourOpen(!tourOpen)})}
+      {playgroundSetup({
+        buttonText: 'Toggle Tour',
+        onButtonClick: () => setTourOpen(!tourOpen)
+      })}
       <Walktour
         steps={secondarySteps()}
-        identifier={"2"}
-        rootSelector={"#demo-container"}
+        identifier={'2'}
+        rootSelector={'#demo-container'}
       />
       <Walktour
         steps={primaryIntoSecondary()}
-        identifier={"1"}
+        identifier={'1'}
         isOpen={tourOpen}
-        customCloseFunc={(logic: WalktourLogic) => { setTourOpen(false); logic.close(); }}
+        customCloseFunc={(logic: WalktourLogic) => {
+          setTourOpen(false);
+          logic.close();
+        }}
         disableCloseOnClick
       />
     </>
-  )
-}
+  );
+};
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const container = document.getElementById('app');
+const root = createRoot(container!);
+root.render(<App />);
